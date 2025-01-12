dd5ff3c70fb5b0151e998519000c4489c368272c
sudipta1910 <shudiptadasjoy200024@gmail.com>
2025-01-12 10:25:53 +0600

    network

--- lib/services/network_caller/network_caller.dart
+++ lib/services/network_caller/network_caller.dart
@@ -38,14 +38,56 @@ class NetworkCaller {
         );
       }
     } catch (e) {
+      _logResponse(url, -1,null, '', e.toString());
       return NetworkResponse(
           isSuccess: false, statusCode: -1, errorMessage: e.toString());
     }
   }
-  void _logRequest(String url,[ Map<String ,dynamic>? headers,Map<String ,dynamic>? body,]){
+  Future<NetworkResponse> postRequest(String url, {Map<String, dynamic>? body}) async {
+    Uri uri = Uri.parse(url);
+    Map<String, String> headers = {
+      'content-type':'application/json'
+    };
+    _logRequest(url,headers,body);
+    Response response = await post(uri,headers: headers,body: jsonEncode(body));
+    _logResponse(url, response.statusCode, response.headers, response.body);
+    try {
+      if (response.statusCode == 200) {
+        final decodedMessage = jsonDecode(response.body);
+        return NetworkResponse(
+            isSuccess: true,
+            statusCode: response.statusCode,
+            responseData: decodedMessage);
+      } else {
+        return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+        );
+      }
+    } catch (e) {
+      _logResponse(url, -1,null, '', e.toString());
+      return NetworkResponse(
+          isSuccess: false, statusCode: -1, errorMessage: e.toString());
+    }
+  }
+
+  void _logRequest(
+    String url, [
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? body,
+  ]) {
     _logger.i('URL => $url\nHEADERS=>$headers\nBODY=>$body');
   }
-  void _logResponse(String url,int statusCode,Map<String ,String> headers,String body){
-    _logger.i('URL => $url\nHEADERS=>$headers\nStatus Code=>$statusCode\nBODY=>$body');
+
+  void _logResponse(
+      String url, int statusCode, Map<String, String>? headers,String body,[String? errorMessage]) {
+    if(errorMessage != null){
+      _logger.e(
+          'URL => $url\nERROR MESSAGE=>$errorMessage');
+    }else{
+      _logger.i(
+          'URL => $url\nHEADERS=>$headers\nStatus Code=>$statusCode\nBODY=>$body');
+    }
+
   }
 }
